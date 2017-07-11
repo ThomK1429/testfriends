b748bc05b27a41bc3742fa2db03bd9e247d5c9fb
Thomas Keel <sndbytgkv1@verizon.net>
2017-07-10 21:07:33 -0400

    init files

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendSurvey = [
+{
+    name:"Bobby",
+    photo:"https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_960_720.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+
+];
+
+module.exports = friendSurvey;
